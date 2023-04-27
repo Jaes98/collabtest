680b6bcc0e65e16dc6516106bf63e82308d1d3cb
Jasa95 <jaco3020@stud.kea.dk>
2023-04-27 09:20:52 +0200

    added text (jacob)

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 window.addEventListener("load", initApp);
 
 function initApp() {
-console.log("hello world");
+  console.log("hello world");
 
-console.log("hello peter");
+  console.log("hello Marcus");
+
+  console.log("Mit navn er jacob");
 }
-
-
